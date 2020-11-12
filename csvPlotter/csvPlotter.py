61669cdbecede2e5289322ce034e978140a279a6
sarvparteek <sarvps@umich.edu>
2020-11-11 21:27:31 -0500

    Add files for plotting and printing sprint results

---
+++ csvPlotter/csvPlotter.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
+from reader import Reader
+
+dataframe = pd.read_csv('/home/sarv/Downloads/Reading Diaries - Sheet2.csv')
+target = 10
+readers = [Reader(dataframe['Sarv'], target), Reader(dataframe['Ashish'], target), Reader(dataframe['Ankit'], target),
+           Reader(dataframe['Richa'], 5)]
+names = []
+total_pages = []
+mean_pages = []
+violations = []
+eff_violations = []
+fines = []
+
+for r in readers:
+    r.process()
+    names.append(r.name)
+    total_pages.append(r.total_pages)
+    mean_pages.append(r.mean_pages)
+    violations.append(r.violations)
+    eff_violations.append(r.eff_violations)
+    fines.append(r.fines)
+
+plt.close('all')
+
+fig = plt.figure(1)
+fig.clf()
+for r in readers:
+    plt.plot(dataframe['Date'], getattr(r, 'data'), '-o', label=getattr(r, 'name'))
+plt.plot(dataframe['Date'], target * np.ones(dataframe['Date'].size), linestyle = '--', label = 'Target')
+plt.xlabel('Date')
+plt.ylabel('Pages/day')
+plt.title('Reading diaries')
+plt.legend()
+#plt.grid()
+
+def plot_data(fig_num, x_labels, y_label, data, grid_type = 'major'): # use 'both' for fine grid lines
+    fig = plt.figure(fig_num)
+    fig.clf()
+    y_pos = np.arange(len(x_labels))
+    plt.bar(y_pos, data, align='center')
+    plt.xticks(y_pos, x_labels)
+    plt.ylabel(y_label)
+    plt.grid(b=True, which=grid_type)
+    plt.minorticks_on()
+
+plot_data(2, names, 'Violations', violations)
+plot_data(3, names, 'Effective violations', eff_violations)
+plot_data(4, names, 'Fines', fines)
+plot_data(5, names, 'Total pages', total_pages)
+plot_data(6, names, 'Mean pages', mean_pages)
+
+print(dataframe)
+
+pdf = PdfPages("Sprint2Results.pdf")
+for fig in plt.get_fignums(): ## will open an empty extra figure :(
+    pdf.savefig(fig)
+pdf.close()
+plt.show()
+print("Hello")
---
+++ csvPlotter/reader.py
@@ -0,0 +1,45 @@
+class Reader:
+    def __init__(self, series, daily_target=10):
+        self.data = series  # pandas series
+        self.name = series.name
+        self.week_length = 7
+        self.week1 = dict() # keys: violations, eff_violations
+        self.week2 = dict() # keys: violations, eff_violations
+        self.mean_pages = 0
+        self.total_pages = 0
+        self.fine_per_violation = 5
+        self.fines = 0
+        self.violations = 0
+        self.eff_violations = 0
+        self.daily_target = daily_target
+        
+    def computeWeekViolations(self, data, results):
+        results["violations"] = 0
+
+        # There is a violation if the daily target is not achieved on two consecutive days
+        for i in range(self.week_length - 1):
+            if data.iloc[i] < self.daily_target and data.iloc[i+1] < self.daily_target:
+                results["violations"] += 1
+
+        # If pages worth daily_target times number of days in the week are read by the end of the week, then the
+        # effective number of violations are zero.
+        if data.sum() >= self.week_length * self.daily_target:
+            results["eff_violations"] = 0
+        else:
+            results["eff_violations"] = results["violations"]
+
+    def process(self):
+        self.computeWeekViolations(self.data.head(self.week_length), self.week1)
+        self.computeWeekViolations(self.data.tail(self.week_length), self.week2)
+        self.total_pages = self.data.sum()
+        self.mean_pages = self.total_pages / (2 * self.week_length)
+        self.violations = self.week1["violations"] + self.week2["violations"]
+        self.eff_violations = self.week1["eff_violations"] + self.week2["eff_violations"]
+        # Weekly violation counting system skips the case where someone didn't read on the last day of week 1 and first
+        # day of week 2
+        if self.data.iloc[self.week_length - 1] < self.daily_target and \
+           self.data.iloc[self.week_length] < self.daily_target:
+            self.violations += 1
+            self.eff_violations += 1
+
+        self.fines = self.fine_per_violation * self.eff_violations
